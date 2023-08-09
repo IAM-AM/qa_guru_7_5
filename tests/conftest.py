531fe2320d7ceeef5d04319736cb54d91ab2d7aa
IAM-AM <aliaksandr.manko1@gmail.com>
2023-08-09 23:32:17 +0400

    Updated conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,11 @@ from selene import browser
 
 @pytest.fixture(scope="function", autouse=True)
 def set_browser_windows_size():
+    browser.config.base_url = "https://demoqa.com/"
     browser.config.window_height = 2560
     browser.config.window_width = 1440
+    browser.open()
+
     yield
+
     browser.quit()
