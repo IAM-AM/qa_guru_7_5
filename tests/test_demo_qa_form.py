35e235e9eddb0b3cf945adb8e8a78ffa2efead94
IAM-AM <aliaksandr.manko1@gmail.com>
2023-08-10 23:53:25 +0400

    Updated test

--- tests/test_demo_qa_form.py
+++ tests/test_demo_qa_form.py
@@ -8,5 +8,4 @@ def test_student_sign_up():
     browser.element('#lastName').type('Sparrow')
     browser.element('#userEmail').type('Jack@pirate.com')
 
-
-
+    browser.element('#gender-radio-1').with_(timeout=browser.config.timeout * 1.5).click()
