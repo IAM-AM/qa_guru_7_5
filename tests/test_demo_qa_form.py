7abba8d8d7f7c81366aba5bea3b4ce79a24f5416
IAM-AM <aliaksandr.manko1@gmail.com>
2023-08-12 23:39:02 +0400

    Updated test

--- tests/test_demo_qa_form.py
+++ tests/test_demo_qa_form.py
@@ -1,4 +1,6 @@
-from selene import be, have, browser
+from selene import browser, command, have
+from selenium.webdriver import Keys
+import os
 
 
 def test_student_sign_up():
@@ -7,5 +9,18 @@ def test_student_sign_up():
     browser.element('#firstName').type('Jack')
     browser.element('#lastName').type('Sparrow')
     browser.element('#userEmail').type('Jack@pirate.com')
+    browser.element('#userNumber').type('+9901234567890')
 
-    browser.element('#gender-radio-1').with_(timeout=browser.config.timeout * 1.5).click()
+    browser.element('label[for="gender-radio-1"]').click()
+    browser.element('#dateOfBirthInput').perform(command.js.scroll_into_view).click()
+    browser.element('select[class="react-datepicker__year-select"]').click().element(
+        'option[value="1900"]').click()
+    browser.element('select[class="react-datepicker__month-select"]').click().element(
+        'option[value="5"]').click()
+    browser.element('div[class="react-datepicker__day react-datepicker__day--020"]').click()
+
+
+    browser.element('#subjectsInput').type('Economics').press_tab()
+    browser.element('label[for="hobbies-checkbox-2"]').click()
+
+
